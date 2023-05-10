a08852ccc5e8f97ef942173ebb09faa3b2a34da4
Fazkek <125206279+Fazkek@users.noreply.github.com>
2023-05-10 17:02:58 +0400

    Add files via upload

---
+++ attempt_3 — копия/test_color_predict.py
@@ -0,0 +1,109 @@
+data = {
+    '1161388698': [
+        {'рубашка': 'фиолетовый'}, {'футболка': 'светло-голубой'},
+        {'футболка': 'сиреневый'}, {'брюки': 'неопределённый'},
+        {'рубашка': 'светло-голубой'}
+    ]
+}
+
+for i in range(len(data['1161388698'])):
+            for key, value in data['1161388698'][key].items():
+                print(f"{key} {key}: {value}")
+                if color == 'синий':
+                    for key in s:
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'белый':
+                            print('синие брюки и белая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'желтый':
+                            print('синие брюки и желтая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'черный':
+                            print('синие брюки и черная', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'фиолетовый':
+                            print('синие брюки и фиолетовая', key)
+                            break
+                if color == 'желтый':
+                    for key in s:
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'белый':
+                            print('желтые брюки и белая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'сиреневый':
+                            print('желтые брюки и сиреневая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'голубой':
+                            print('желтые брюки и голубая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'черный':
+                            print('желтые брюки и черная', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'фиолетовый':
+                            print('желтые брюки и фиолетовая', key)
+                            break
+                if color == 'красный':
+                    for key in s:
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'белый':
+                            print('красные брюки и белая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'желтый':
+                            print('красные брюки и желтая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'черный':
+                            print('красные брюки и черная', key)
+                            break
+                if color == 'оранжевый':
+                    for key in s:
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'белый':
+                            print('оранжевые брюки и белая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'черный':
+                            print('оранжевые брюки и черная', key)
+                if color == 'зеленый':
+                    for key in s:
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'белый':
+                            print('зеленые брюки и белая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'сиреневый':
+                            print('зеленые брюки и сиреневая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'черный':
+                            print('зеленые брюки и черная', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'синий':
+                            print('зеленые брюки и синяя', key)
+                            break
+                if color == 'фиолетовый':
+                    for key in s:
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'белый':
+                            print('фиолетовые брюки и белая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'сиреневый':
+                            print('фиолетовые брюки и сиреневая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'черный':
+                            print('фиолетовые брюки и черная', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'желтый':
+                            print('фиолетовые брюки и желтая', key)
+                            break
+                if color == 'розовый':
+                    for key in s:
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'белый':
+                            print('розовые брюки и белая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'сиреневый':
+                            print('розовые брюки и сиреневая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'черный':
+                            print('розовые брюки и черная', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'зеленый':
+                            print('розовые брюки и зеленая', key)
+                            break
+                        if key != 'брюки' and key != 'платье' and key != 'ботинки' and value == 'синий':
+                            print('розовые брюки и синяя', key)
+                            break
+                if color == 'черный':
+                    print('черные вещи подходят ко всему, так что ориентируйтесь только на погодные условия')
+                if color == 'белый':
+                    print('белые вещи подходят ко всему, так что ориентируйтесь только на погодные условия')
